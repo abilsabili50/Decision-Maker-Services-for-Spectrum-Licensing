fd7bc161255ed832d37e3d082fe496e8ba3e3c74
Abilsabili <abilsabili50@gmail.com>
2023-12-17 03:57:47 +0700

    hot-fix: deleting request.py

--- request.py
+++
@@ -1,5 +0,0 @@
-columns = ['SUBSERVICE', 'UMUR_ISR', 'BWIDTH', 'CIRCUIT_LEN', 'EQ_PWR', 'ERP_PWR_DBM', 'FREQ',
-            'FREQ_PAIR', 'GAIN', 'HGT_ANT', 'LAT_DEG', 'LAT_MIN',
-            'LAT_SEC', 'LONG_DEG', 'LONG_MIN', 'LONG_SEC', 'LOSS', 'SID_LAT', 'SID_LONG']
-
-categorical_columns = ['SUBSERVICE']
